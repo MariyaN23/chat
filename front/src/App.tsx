2c8cc1dc764863bea1b17509e62a61495bba0e3f
MariyaN23 <sellaite505@gmail.com>
2024-08-21 17:46:36 +0300

    message sent, name changed

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {io} from "socket.io-client";
+
+type UserType = {
+    userId: string
+    name: string
+}
+
+type MessageType = {
+    message: string
+    messageId: string
+    user: UserType
+}
+
+export type AllMessagesType = MessageType[]
+
+const socket = io('http://localhost:3009/')
 
 function App() {
-    const [messages, setMessages] = React.useState([
-        {message: 'Hello', messageId: '545sdsd4sd', user: {userId: '45sdgsddd', name: 'Dimych'}},
-        {message: 'Hello, world', messageId: '548ahgfad47', user: {userId: 'ada11311de', name: 'Oleg'}},
-    ])
+    useEffect(() => {
+        socket.on('init-messages-published', initMessagePublished)
+        socket.on('new-message-sent', newMessageSent)
+        return () => {
+            socket.off('init-messages-published', initMessagePublished)
+            socket.off('new-message-sent', newMessageSent)
+        }
+    }, [])
+
+    const initMessagePublished = (messages: AllMessagesType) => {
+        setAllMessages(messages)
+    }
+    const newMessageSent = (message: MessageType) => {
+        setAllMessages((prevMessages) => [...prevMessages, message])
+    }
+
+    const [allMessages, setAllMessages] = useState<AllMessagesType>([])
+
+    const [message, setMessage] = useState('')
+
+    const sendMessageHandler = () => {
+        if (message.trim().length) {
+            socket.emit('client-message-sent', message)
+        }
+        setMessage('')
+    }
+
+    const [name, setName] = useState('')
+
+    const sendNameHandler = () => {
+        if (name.trim().length) {
+            socket.emit('client-name-set', name)
+        }
+    }
+
+    useEffect(() => {
+
+    }, [allMessages])
+
     return (
         <div className="App">
             <div>
@@ -17,15 +68,26 @@ function App() {
                         width: '300px',
                         overflowY: 'scroll'
                     }}>
-                        {messages.map(m => {
-                            return <div>
+                        {allMessages.map(m => {
+                            return <div key={m.messageId}>
                                 <b>{m.user.name}:</b> {m.message}
                                 <hr/>
                             </div>
                         })}
                     </div>
-                    <textarea></textarea>
-                    <button>Send</button>
+
+                    <div>
+                        <input value={name} onChange={(e) => setName(e.currentTarget.value)}/>
+                        <button onClick={sendNameHandler}>Name in chat</button>
+                    </div>
+
+                    <div>
+                        <textarea value={message}
+                                  onChange={(e) => setMessage(e.currentTarget.value)}>
+                    </textarea>
+                        <button onClick={sendMessageHandler}>Send</button>
+                    </div>
+
                 </div>
             </div>
         </div>
